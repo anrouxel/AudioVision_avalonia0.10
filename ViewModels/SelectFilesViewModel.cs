8be4aff9686390babbe9d9babea7a06328fa4e3b
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-02-20 13:13:30 +0100

    Update

---
+++ ViewModels/SelectFilesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ReactiveUI;
+
+namespace AudioVision.ViewModels;
+
+public class SelectFilesViewModel : ViewModelBase
+{
+    private bool isFileSelected;
+    private bool isDiscSelected;
+
+    public bool IsFileSelected
+    {
+        get => isFileSelected;
+        set => this.RaiseAndSetIfChanged(ref isFileSelected, value);
+    }
+
+    public bool IsDiscSelected
+    {
+        get => isDiscSelected;
+        set => this.RaiseAndSetIfChanged(ref isDiscSelected, value);
+    }
+}
---
+++ Views/SelectFilesView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AudioVision.Views;
+
+public partial class SelectFilesView : UserControl
+{
+    public SelectFilesView()
+    {
+        InitializeComponent();
+    }
+}
