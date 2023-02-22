f461f853096dd607a7fb631bdd9489f44681de13
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-02-22 14:37:20 +0100

    Update SelectFilesView

--- ViewModels/SelectFilesViewModel.cs
+++ ViewModels/SelectFilesViewModel.cs
@@ -1,21 +1,67 @@
 ﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.IO;
+using Avalonia.Controls;
+using System.Linq;
 
 namespace AudioVision.ViewModels;
 
 public class SelectFilesViewModel : ViewModelBase
 {
-    private bool isFileSelected;
-    private bool isDiscSelected;
-
+    private bool _isFileSelected;
+    private bool _isDiscSelected;
+    private ObservableCollection<FileInfo> _selectedFiles;
+    
     public bool IsFileSelected
     {
-        get => isFileSelected;
-        set => this.RaiseAndSetIfChanged(ref isFileSelected, value);
+        get => _isFileSelected;
+        set => this.RaiseAndSetIfChanged(ref _isFileSelected, value);
     }
 
     public bool IsDiscSelected
     {
-        get => isDiscSelected;
-        set => this.RaiseAndSetIfChanged(ref isDiscSelected, value);
+        get => _isDiscSelected;
+        set => this.RaiseAndSetIfChanged(ref _isDiscSelected, value);
+    }
+
+    public ObservableCollection<FileInfo> SelectedFiles
+    {
+        get => _selectedFiles;
+        set => this.RaiseAndSetIfChanged(ref _selectedFiles, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> AddFilesCommand { get; }
+    public ReactiveCommand<FileInfo, Unit> DeleteFilesCommand { get; }
+
+    public ObservableCollection<DriveInfo> AvailableDrives { get; }
+
+    public SelectFilesViewModel()
+    {
+        _selectedFiles = new ObservableCollection<FileInfo>();
+        _isFileSelected = true;
+        IsDiscSelected = false;
+        AddFilesCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            var dialog = new OpenFileDialog();
+            dialog.AllowMultiple = true;
+            var window = new Window();
+            var result = await dialog.ShowAsync(window);
+            if (result != null)
+            {
+                foreach (var item in result)
+                {
+                    SelectedFiles.Add(new FileInfo(item));
+                }
+            }
+        });
+        DeleteFilesCommand = ReactiveCommand.Create<FileInfo>((file) =>
+        {
+            if (SelectedFiles.Contains(file))
+            {
+                SelectedFiles.Remove(file);
+            }
+        });
+        AvailableDrives = new ObservableCollection<DriveInfo>(DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.CDRom));
     }
 }
